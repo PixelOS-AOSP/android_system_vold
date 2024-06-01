4e6d1bce12a782eb2be4c1fabb1c3bf6907d3756
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-01 23:06:41 +0300

    add API for destroying metadata encryption key
    It's used in the extended wipe-without-reboot implementation.
    
    Change-Id: Ieb5dad91c7b7bf3f51b8e134cd394acacfe01e4e

--- VoldNativeService.h
+++ VoldNativeService.h
@@ -176,6 +176,7 @@ class VoldNativeService : public BinderService<VoldNativeService>, public os::Bn
     binder::Status getWriteBoosterLifeTimeEstimate(int32_t* _aidl_return);
 
     binder::Status checkNonCeStorageKeys(std::vector<std::string>* _aidl_return) override;
+    binder::Status destroyMetadataKey(const std::string& mountPointPath) override;
 };
 
 }  // namespace vold
